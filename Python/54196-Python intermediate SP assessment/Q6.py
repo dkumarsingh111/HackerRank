0ae104ff87fb5af75208bed95f89fe735ca3902b
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-07-15 20:47:21 +0530

    add file via upload

---
+++ Python/54196-Python intermediate SP assessment/Q6.py
@@ -0,0 +1,13 @@
+print(type((100-4*5)/(20-2*5)))
+print(type(7.0/4))
+print(type(5/4))
+print(type(2*4/2))
+print(type(5-2))
+
+
+#Output:
+    # <class 'float'>
+    # <class 'float'>
+    # <class 'float'>
+    # <class 'float'>
+    # <class 'int'>  
