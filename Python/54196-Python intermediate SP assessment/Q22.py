42c2920c4942445de2579530d18515033e147249
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-07-16 20:37:25 +0530

    add file via upload

---
+++ Python/54196-Python intermediate SP assessment/Q22.py
@@ -0,0 +1,29 @@
+def factorial1(N):
+    return N if N == 1 else N * fact0(N-1)
+
+
+def factorial2(N):
+    res = 1
+    for i in range(1, N+1):
+        res *= i
+    return res
+
+
+def factorial3(N):
+    if N == 1:
+        return N
+    else:
+        return N * fact0(N-1)
+
+        
+
+def fact0(X):
+    res = 1
+    for i in range(1, X+1):
+        res *= i
+    return res
+
+
+print(factorial1(10))
+print(factorial2(10))
+print(factorial3(10))
