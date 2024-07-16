5ee1875e58c57ff32ba6c6f8b922fc64776ccd54
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-07-16 19:33:49 +0530

    add file via upload

--- Python/54196-Python intermediate SP assessment/Q12.py
+++ Python/54196-Python intermediate SP assessment/Q12.py
@@ -1,11 +1,9 @@
 class A:
-
     def __init__(self, i=1):
         self.i = i
 
 
 class B(A):
-
     def __init__(self, j=2):
         super().__init__()
         self.j = j
@@ -14,4 +12,7 @@ class B(A):
         b = B()
         print(b.i, b.j)
 
-    main()
+
+B.main()
+
+#Output: 1 2
