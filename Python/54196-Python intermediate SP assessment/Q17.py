ed61f05b6ef95a12f0f6075369fa6d465fadd3a0
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-07-16 19:48:03 +0530

    add file via upload

---
+++ Python/54196-Python intermediate SP assessment/Q17.py
@@ -0,0 +1,15 @@
+# sum( sq(for i in range(10)))
+# def sq(n):
+#     return n*n
+
+# for i in range(10):
+#     sum=sum+sq(i)
+
+
+#     def sq(n):
+#         return n*n
+
+
+# sum([i**2 for i in range(10))
+
+print(sum([i**2 for i in range(10)]))
