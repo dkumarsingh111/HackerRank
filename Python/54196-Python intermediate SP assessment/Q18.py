1abc3c3aa4e1d5eac6e78531dc22828492f5098a
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-07-16 20:01:11 +0530

    add file via upload

---
+++ Python/54196-Python intermediate SP assessment/Q18.py
@@ -0,0 +1,11 @@
+class A:
+    def __init__(self, param):
+        self.a1=param
+
+
+class B(A):
+    def __init__(self, param):
+        self.b1=param
+
+        obj=B(100)
+        print("%d %d" % (obj.a1, obj.b1))
