3a39e2d9f13d7309b1b4ef75aff291b5e3cee82b
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-07-19 15:35:29 +0530

    add file via upload

---
+++ Python/54196-Python intermediate SP assessment/Q14.py
@@ -0,0 +1,12 @@
+#what is he value of variable 'k' in the below shown code?
+
+k = [print(i) for i in 'hello' if i not in 'aeiou']
+
+print(k)
+
+
+#Output:
+        # h
+        # l
+        # l
+        # [None, None, None]
