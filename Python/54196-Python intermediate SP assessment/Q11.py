7519cd07d028b3fd64bc6a0b6dd267e3cfcdcacc
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-07-19 15:29:33 +0530

    add file via upload

---
+++ Python/54196-Python intermediate SP assessment/Q11.py
@@ -0,0 +1,19 @@
+for i in range(1,6):
+    print(i)
+    if i%4==0:
+        print('x')
+
+    elif i%2==0:
+        print('x')
+
+    else:
+        print('xxxx')
+
+
+
+#Output:
+        # xxxx
+        # x   
+        # xxxx
+        # x   
+        # xxxx
