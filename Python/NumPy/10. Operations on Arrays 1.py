5e2e6ac2de4e72bae0e1baad5b9de92042240b32
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-08-02 16:54:09 +0530

    add file via upload

--- Python/NumPy/10. Operations on Arrays 1.py
+++ Python/NumPy/10. Operations on Arrays 1.py
@@ -1,2 +1,20 @@
 #Welcome to Operations on Arrays 1
 
+import numpy as np
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def array_oper(num1,num2):
+       
+    y= np.arange(num1,num2+1)
+    print(y)
+    y=y.reshape(2,3)
+    y=y**2
+    y=y+5
+    print(y)
+    
+    
+
+if __name__ == "__main__":
+    num1 = int(input())
+    num2 = int(input())
+    array_oper(num1,num2)
