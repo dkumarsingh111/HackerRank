13831f93843e6ced077caf1574dafd064fdc9ba5
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-08-05 19:12:54 +0530

    add file via upload

---
+++ Matplotlib/Quiz-2.py
@@ -0,0 +1,16 @@
+#Which of the following statements is correct, based on figure generated with below code?
+
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(111, title="My Plot")
+x = [0, 1, 2, 3, 4]
+y = [0, 3, 6, 9, 12]
+plt.scatter(x, y, s=[20, 60], c=['r', 'g'])
+plt.show()
+
+
+# A. Alteranative data points are marked with different color and size
+# B. Results in Error
+# C. All data points are marked in green color with size 60
+# D. All data points are marked in red color with size 20   
