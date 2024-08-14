2ce477e80b3b0868628b020ee1c264d78212061d
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-08-14 17:04:09 +0530

    add file via upload

---
+++ Python/SciKit-learn/6. Clustering.py
@@ -0,0 +1,14 @@
+# Welcome to Machine Learning Using Scikit-Learn | 6 | Clustering
+
+#Write your code here
+from sklearn import metrics
+from sklearn import datasets
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering 
+from sklearn.cluster import AffinityPropagation
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.metrics import homogeneity_score
+
+# pls try yourself, if facing any error comment below.
+
