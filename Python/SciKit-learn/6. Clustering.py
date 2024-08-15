5a32dd81065c275590c74626ea5b1cdf8926155f
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-08-15 14:55:31 +0530

    add file via upload

--- Python/SciKit-learn/6. Clustering.py
+++ Python/SciKit-learn/6. Clustering.py
@@ -10,5 +10,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 from sklearn.metrics import homogeneity_score
 
-# pls try yourself, if facing any error comment below.
+# Task 1
+iris = load_iris()
 
+km_cls = KMeans(n_clusters=3, random_state=42)
+km_labels = km_cls.fit_predict(iris.data)
+
+km_h_score = homogeneity_score(iris.target, km_labels)
+
+print('{:.4f}'.format(km_h_score))
+
+# Task 2
+agg_cls = AgglomerativeClustering(n_clusters=3)
+agg_labels = agg_cls.fit_predict(iris.data)
+
+agg_h_score = homogeneity_score(iris.target, agg_labels)
+
+print('{:.4f}'.format(agg_h_score))
+
+# Task 3
+af_cls = AffinityPropagation()
+af_labels = af_cls.fit_predict(iris.data)
+
+af_h_score = homogeneity_score(iris.target, af_labels)
+
+print('{:.4f}'.format(af_h_score))
