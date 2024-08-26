5725bd4e46f981e44995e0bc22e1ac9f3c6b4108
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-08-26 18:53:45 +0530

    add file via upload

--- Python/Stats/test.py
+++ Python/Stats/test.py
@@ -1,2 +1,7 @@
-from scipy import stats
-print(stats.mode([8, 9, 8, 7, 9, 6, 7, 6]))
+import numpy as np
+
+np.random.seed(100)
+print(np.random.rand())
+
+np.random.seed(100)
+print(np.random.rand())
