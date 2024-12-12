f52752098c045b0eb9edc36f327a851d7867ddb1
dkumarsingh111 <dkumarsingh111@gmail.com>
2024-12-12 12:03:55 +0530

    Upload via Visual Studio

---
+++ Python/NLP/11. CountVectorizer Operation.py
@@ -0,0 +1,15 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+document = ["One Geek helps Two Geeks", "Two Geeks help Four Geeks", "Each Geek helps many other Geeks at GeeksforGeeks."]
+
+#Create a Vectorizer Object
+vect = CountVectorizer()
+
+vect.fit(document)
+
+print("Vocabulary:", vect.vocabulary_)
+
+vector = vect.transform(document)
+
+print("Encoded document is:")
+print(vector)
